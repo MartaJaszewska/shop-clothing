479a502bbace0b1d79b221d8e7147c34c79a8c50
MartaJaszewska <jaszewska.marta@gmail.com>
2020-03-03 14:05:57 +0100

    added redux, implement eserReducer and userActions to header and app components

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../FormInput';
 import CustomButton from '../CustomButton';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './style.scss';
 
 class SignIn extends React.Component {
@@ -13,8 +13,14 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+    }
     this.setState({ email: '', password: '' });
   };
 
